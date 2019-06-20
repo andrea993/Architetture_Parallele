ac2849fa89979254d01479820c7d003a2ceb2a56
Andrea993 <andrea@smicro.it>
2019-06-20 11:35:45 +0200

    Matrix_mult_GPGPU_shared

--- Matrix_mult_GPGPU/Matrix_mult.cu
+++ Matrix_mult_GPGPU/Matrix_mult.cu
@@ -1,5 +1,14 @@
 #include <cuda.h>
 #include <stdio.h>
+#include <sys/time.h>
+
+inline double nowSec()
+{
+	struct timeval t;
+	struct timezone tzp;
+	gettimeofday(&t, &tzp);
+	return t.tv_sec + t.tv_usec*1e-6;
+}
 
 __host__ void host_mmul (int *A, int *B, int *C, int N)
 {
@@ -25,6 +34,7 @@ __global__ void global_mmul (int *A, int *B, int *C, int N)
 	}	
 }
 
+#ifdef PRINT
 void printMtx (int *m, int N)
 {
 	for (int i=0; i<N*N; i++)
@@ -37,12 +47,20 @@ void printMtx (int *m, int N)
 		m++;
 	}
 }
+#endif
 
 
-int main()
+int main(int argc, char **argv)
 {
-	int N=10;
+	if (argc != 2)
+	{
+		puts("Usage: Matrix_mult [N]\n");
+		return -1;
+	}
+	
+	int N=atoi(argv[1]);
 	int NN=N*N;
+	int i;
 	int *A, *B, *C_host, *C_device;
 	int *ptrA, *ptrB;
 
@@ -52,7 +70,7 @@ int main()
 	cudaMallocManaged(&C_host, NN*sizeof(int));
 	cudaDeviceSynchronize(); //attende che la memoria sia allocata
 
-	for (int i=0, ptrA=A, ptrB=B ; i<N; i++)
+	for (i=0, ptrA=A, ptrB=B ; i<N; i++)
 	{
 		for (int j=0; j<N; j++)
 		{
@@ -66,27 +84,41 @@ int main()
 		}
 	}
 
+#ifdef PRINT
 	fprintf(stderr,"A=\n");
 	printMtx(A, N);
 	fprintf(stderr,"\n\nB=\n");
 	printMtx(B, N);
 	fprintf(stderr,"\n\n");
+#endif
 
+	double t_begin_cpu = nowSec();
 	host_mmul(A, B, C_host, N);
+	double t_end_cpu = nowSec();
 
 	dim3 blockPerGrid(1,1);
 	dim3 threadPerBlock(10,10);
 
+	double t_begin_gpu = nowSec();
 	global_mmul <<< blockPerGrid, threadPerBlock >>> (A,B,C_device,N);
-
 	cudaDeviceSynchronize();
-
+	double t_end_gpu = nowSec();
+	
+#ifdef PRINT
 	fprintf(stderr,"C_host=\n");
 	printMtx(C_host, N);
 	fprintf(stderr,"\n\nC_device=\n");
 	printMtx(C_device, N);
 	fprintf(stderr,"\n");
-	// devo usare la memoria shared, ipotizzo che N sia una potenza di 2, prima sceglire come usare i blocchi poi implementare <- è un ASSIGNMENT
+#endif
+
+	printf("Elapsed time CPU: %f sec\n", t_end_cpu  - t_begin_cpu);
+	printf("Elapsed time GPU: %f sec\n", t_end_gpu  - t_begin_gpu);
+	
+	cudaFree(A);
+	cudaFree(B);
+	cudaFree(C_device);
+	cudaFree(C_host);
 
 	return 0;
 }
